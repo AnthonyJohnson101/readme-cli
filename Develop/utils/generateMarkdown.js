73e7c32627f1ea750803caee77feb32be0e9bd63
AnthonyJohnson101 <tonybologna123@outlook.com>
2023-02-06 17:55:18 -0500

    added init function and readme generator

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,90 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
+const inquirer = require('inquirer');
+const indexFile = require('../index');
 
-// TODO: Create a function that returns the license link
+//function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  let badge = '';
+  if (license == 'MIT') {
+    badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+  } else if (license == 'Apache'){
+    badge = '[![License: GPL v3](https://img.shields.io/badge/License-Apache-yellow.svg)'
+  } else if (license == 'Mozilla'){
+    badge = '[![License](https://img.shields.io/badge/License-Mozilla-yellow.svg)'
+  } else{
+    badge = ""
+  }
+  return badge;
+}
 
-// TODO: Create a function that returns the license section of README
+//function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = '';
+  if (license === 'MIT'){
+    licenseLink = 'https://choosealicense.com/licenses/mit'
+  }else if (license === 'Apache'){
+    licenseLink = 'https://www.apache.org/licenses/LICENSE-2.0'
+  } else if (license === 'Mozilla'){
+    licenseLink = 'https://www.mozilla.org/en-US/MPL/2.0/'
+  } else {
+    licenseLink = ""
+  }
+  return licenseLink;
+}
 
-// TODO: Create a function to generate markdown for README
+//function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  let licenseSection = '';
+  if (license === 'None'){
+    licenseSection = ''
+  } else {
+    licenseSection = `${license}`
+  }
+  return licenseSection;  
+}
+
+//function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-`;
+  ## ${renderLicenseBadge(data.license)}
+
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents
+  ###  [Installation](#installation)
+  ###  [Usage](#usage)
+  ###  [Contributors](#contributions)
+  ###  [Tests](#test)
+  ###  [License](#license)
+  ###  [Questions](#user)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributors
+  ${data.contributions}
+
+  ## Test Performed
+  ${data.test}
+
+  ## License
+  ## ${renderLicenseSection(data.license)}
+  ## ${renderLicenseLink(data.license)}
+
+  ## Questions
+  ### If you have any questions, reach out here:
+  Github: ${data.user}
+  Email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
